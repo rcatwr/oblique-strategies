282eca41701749251e8a091a7704fe7caf069e5f
Rich C <richard.minardi@gmail.com>
2019-04-23 17:06:01 -0700

    added bulma

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import oblique from './data/oblique';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button, Heading } from 'react-bulma-components'
 
 class App extends Component {
   constructor(){
@@ -24,14 +26,14 @@ class App extends Component {
     return (
       <div className="wrapper">
         <div className="header">
-          <h1>OBLIQUE STRATEGIES</h1>
+          <Heading>OBLIQUE STRATEGIES</Heading>
           <p>Over one hundred worthwhile dilemmas</p>
 
         </div>
       <div className="card">
         <p>{this.state.card}</p>
         </div>
-        <button onClick={this.handleChange}>draw</button>
+        <Button onClick={this.handleChange}>draw</Button>
 
         <p className="source">Oblique Strategies © 1975, 1978, and 1979 Brian Eno/Peter Schmidt </p>
         <p className="source"><a href="https://en.wikipedia.org/wiki/Oblique_Strategies">Wikipedia</a></p>
