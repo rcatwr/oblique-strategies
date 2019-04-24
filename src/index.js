79a001a94d794ed15748971bc60caadbd556e217
Rich C <richard.minardi@gmail.com>
2019-04-24 13:26:22 -0700

    refine bulma style

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Webfont from 'webfontloader';
 import * as serviceWorker from './serviceWorker';
+
+Webfont.load({
+  google: {
+    families: ['Open Sans:300,400,700','Quicksand:300,400,700', 'sans-serif'] 
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
